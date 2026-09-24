ecc1e122a641c001862cc1a1cdb2b607a43b12c9
jlaltamar05 <jlaltamar01@gmail.com>
2026-09-24 14:48:21 -0400

    Delete Sistema-Contable directory

--- Sistema-Contable/src/routes/companias.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const c = require('../controllers/companias.controller');
-
-router.get('/', c.listarCompanias);
-router.get('/:id', c.obtenerCompania);
-
-module.exports = router;
--- Sistema-Contable/src/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Servidor del Sistema Contable corriendo en http://localhost:${PORT}`);
-});
